54d2f0417eb6acc1290f3d06682791447f126aac
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-06 13:21:55 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,9 +3,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ GoPay: 085828764046 [rzy-botz]
-├ Dana: 085828764046 [rzy-botz]
-├ Pulsa: 085828764046 [rzy-botz]
+├ GoPay: 085706735450 [Verdi-botz]
+├ Dana: 085706735450 [verdi-botz]
+├ Pulsa: 085706735450 [verdi-botz]
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
